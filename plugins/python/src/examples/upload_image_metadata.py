6d15bd0a332dc26e6bff99e14bc62bfb6ba23f02
Maxim <max@supervise.ly>
2020-04-29 18:45:06 +0300

    [public api] hotfix - image info deserialization

--- plugins/python/src/examples/upload_image_metadata.py
+++ plugins/python/src/examples/upload_image_metadata.py
@@ -1,7 +1,7 @@
 import supervisely_lib as sly
 
-project_id = 110
-dataset_id = 117
+project_id = 75563
+dataset_id = 317675
 
 image_path = '/workdir/src/examples/d.jpg'
 image_name = sly.fs.get_file_name_with_ext(image_path)
@@ -16,6 +16,7 @@ if api.image.get_info_by_name(dataset_id, image_name) is not None:
 
 meta = {"xxx": 777, "yyy": 999}
 uploaded_image_info = api.image.upload_path(dataset_id, image_name, image_path, meta)
+print("!!! uploaded_image_info", uploaded_image_info)
 print("[FOR DEBUG] Image meta on server: ", uploaded_image_info.meta)
 
 image_info = api.image.get_info_by_name(dataset_id, image_name)
