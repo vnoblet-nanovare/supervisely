a69ea2fe02e4488bb10aedc3fd5bd078e60f9158
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-08-11 15:36:58 +0300

    [api] add reviewer_id and reviewer_login to LabelingJob  info

--- plugins/python/src/examples/unsorted/lj_create_with_reviewer.py
+++ plugins/python/src/examples/unsorted/lj_create_with_reviewer.py
@@ -4,7 +4,12 @@ print("start")
 
 api = sly.Api.from_env()
 
-lj_info = api.labeling_job.create(name="lj_with_reviewer", dataset_id=808, user_ids=[1], reviewer_id=6)
-print(lj_info)
+#lj_info = api.labeling_job.create(name="lj_with_reviewer", dataset_id=808, user_ids=[1], reviewer_id=6)
+#print(lj_info)
+
+info = api.labeling_job.get_info_by_id(id=32)
+print(info.reviewer_id)
+print(info.reviewer_login)
+
 
 print("end")
